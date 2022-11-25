6a139e0151178c9f555155472316c27c8fa45c1f
reyl <nolan.finck.nf@gmail.com>
2022-11-26 00:07:11 +0100

    slash commands avaible

--- bot.py
+++ bot.py
@@ -1,61 +1,60 @@
 import discord
+from discord import app_commands
 from dictionnary import quotes
-from discord.ext import commands
 from random import randint
 from info import token
-from discord_slash import SlashCommand
-
-client = commands.Bot(command_prefix='=', help_command=None)
-slash = SlashCommand(client, sync_commands=True)
-
-@client.event
-async def on_ready():
-    print('Le bot est démarré en tant que {0.user}'.format(client))
-    print('==========================')
 
 
-# Répond "Salut !"
-@client.command()
-async def hello(ctx):
-    await ctx.send(f"Hello {discord.Member}")
+class GrandSage(discord.Client):
+    def __init__(self):
+        super().__init__(intents=discord.Intents.default())
+        self.synced = False
+
+    async def on_ready(self):
+        await self.wait_until_ready()
+        if not self.synced:
+            await tree.sync()
+            self.synced = True
+        print(f'Le bot est démarré en tant que {self.user}')
+        print('==========================')
+
+
+client = GrandSage()
+tree = app_commands.CommandTree(client)
 
 
 # Envoie une citation du dictionnaire aléatoire dans le channel
-@slash.slash(name="citation", description="Envoie une citation aléatoire")
-async def citation(ctx):
-    await ctx.message.delete()
-    await ctx.send(f'Un grand sage a dit un jour : ***"{quotes[randint(0, len(quotes)-1)]}"***.')
+@tree.command(name="citation", description="Envoie une citation aléatoire")
+async def citation(interaction: discord.Interaction):
+    await interaction.response.send_message(f'Un grand sage a dit un jour : ***"{quotes[randint(0, len(quotes) - 1)]}"***.')
     print("Citation envoyée.")
 
 
 # Permet d'afficher le dictionnaire de citations
-@slash.slash(name="show_quotes", description="Affiche la liste de citations")
-async def show_quotes(ctx):
-    await ctx.message.delete()
+@tree.command(name="show_quotes", description="Affiche la liste de citations")
+async def show_quotes(interaction: discord.Interaction):
     file = open("dictionnary.py", "r")
-    await ctx.send(file.read())
+    await interaction.response.send_message(file.read())
 
 
 # Permet d'ajouter une citation au dictionnaire
-@slash.slash(name="add_quote", description="Ajoute la citation marquée dans la liste de citations")
-async def add_quote(ctx, text: str):
+@tree.command(name="add_quote", description="Ajoute la citation marquée dans la liste de citations")
+async def add_quote(interaction: discord.Interaction, text: str):
     quotes.append(text)
-    await ctx.message.delete()
     file = open("dictionnary.py", 'w')
     file.write(f"quotes = {quotes}")
-    await ctx.send("Votre citation a été ajoutée au dictionnaire.")
+    await interaction.response.send_message("Votre citation a été ajoutée au dictionnaire.")
     print("Citation ajoutée !")
 
 
 # Permet de supprimer une citation du dictionnaire
-@slash.slash(name="del_quote",
+@tree.command(name="del_quote",
              description="Supprime la citation se trouvant à la position inscrite dans la liste de citations")
-async def del_quote(ctx, index: int):
-    quotes.pop(index-1)
-    await ctx.message.delete()
+async def del_quote(interaction: discord.Interaction, index: int):
+    quotes.pop(index - 1)
     file = open("dictionnary.py", 'w')
     file.write(f"quotes = {quotes}")
-    await ctx.send("Votre citation a bien été enlevé")
+    await interaction.response.send_message("Votre citation a bien été enlevé")
     print("Citation supprimé !")
 
 
